34612d6702495e673d6cadec6a17b79b154a846e
lucasbins <lucas.braga@rede.ulbra.br>
2021-11-30 21:57:06 -0300

    add regra de converter coins

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:koinversor/model/result_coin.dart';
 import 'package:koinversor/service/mercado_bitcoin_service.dart';
 
+import 'package:koinversor/widget/loading.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -10,11 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 List<String> coins = ['BTC', 'LTC', 'ADA', 'UNI', 'USDC'];
-ResultCoin? btc;
-ResultCoin? ltc;
-ResultCoin? ada;
-ResultCoin? uni;
-ResultCoin? usdc;
 
 final btcController = TextEditingController();
 final ltcController = TextEditingController();
@@ -25,20 +22,17 @@ final usdcController = TextEditingController();
 final realController = TextEditingController();
 
 void _clearAll() {
-  realController.text = 1.toString();
-  btcController.text = "";
-  ltcController.text = "";
-  adaController.text = "";
-  uniController.text = "";
-  usdcController.text = "";
+  btcController.text = 1.toStringAsFixed(2);
+  ltcController.text = 1.toStringAsFixed(2);
+  adaController.text = 1.toStringAsFixed(2);
+  uniController.text = 1.toStringAsFixed(2);
+  usdcController.text = 1.toStringAsFixed(2);
 }
 
 Future<List> getData() async {
   List<ResultCoin> list = await Future.wait(
       coins.map((itemId) => MercadoCoinService.fetchCoin(itemId)));
-
   return list.map((response) {
-    // do processing here and return items
     return response;
   }).toList();
 }
@@ -66,12 +60,7 @@ class _HomePageState extends State<HomePage> {
           switch (snapshot.connectionState) {
             case ConnectionState.none:
             case ConnectionState.waiting:
-              return const Center(
-                  child: Text(
-                "Carregando Dados...",
-                style: TextStyle(color: Colors.black, fontSize: 25.0),
-                textAlign: TextAlign.center,
-              ));
+              return const Loading();
             default:
               if (snapshot.hasError) {
                 return const Center(
@@ -91,13 +80,13 @@ class _HomePageState extends State<HomePage> {
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: <Widget>[
                         _dropdown(),
-                        montaTextfield('REAL'),
+                        montaTextfield(_dropdownController),
                         const Text(
                           '=',
                           textAlign: TextAlign.center,
                           style: TextStyle(fontSize: 50),
                         ),
-                        montaTextfield(_dropdownController),
+                        montaTextfield('REAL'),
                       ],
                     ),
                   ),
@@ -130,34 +119,61 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget montaTextfield(String coin) {
+    _clearAll();
     switch (coin) {
-      case 'BTC':
-        return buildTextField(btcController, "R\$ ", _btcChanged);
-      case 'LTC':
-        return buildTextField(ltcController, "R\$ ", _btcChanged);
-      case 'ADA':
-        return buildTextField(adaController, "R\$ ", _btcChanged);
-      case 'UNI':
-        return buildTextField(uniController, "R\$ ", _btcChanged);
-      case 'USDC':
-        return buildTextField(usdcController, "R\$ ", _btcChanged);
       case 'REAL':
+        return buildTextField(realController, "R\$ ", _realChanged);
+      default:
         switch (_dropdownController) {
           case 'BTC':
-            return buildTextField(realController, "BTC ", _btcChanged);
+            return buildTextField(btcController, "BTC ", _btcChanged);
           case 'LTC':
-            return buildTextField(realController, "LTC ", _btcChanged);
+            return buildTextField(ltcController, "LTC ", _ltcChanged);
           case 'ADA':
-            return buildTextField(realController, "ADA ", _btcChanged);
+            return buildTextField(adaController, "ADA ", _adaChanged);
           case 'UNI':
-            return buildTextField(realController, "UNI ", _btcChanged);
+            return buildTextField(uniController, "UNI ", _uniChanged);
           case 'USDC':
-            return buildTextField(realController, "USDC ", _btcChanged);
+            return buildTextField(usdcController, "USDC ", _usdcChanged);
           default:
             return const Text('Error');
         }
-      default:
-        return const Text('Error');
+    }
+  }
+
+  late double btc;
+  late double ltc;
+  late double ada;
+  late double uni;
+  late double usdc;
+
+  double _calculo(a, b, c) {
+    double x = (b * c) / a;
+    return x;
+  }
+
+  void _realChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    switch (_dropdownController) {
+      case 'BTC':
+        btcController.text = _calculo(btc, 1, real).toStringAsFixed(2);
+        break;
+      case 'LTC':
+        ltcController.text = _calculo(ltc, 1, real).toStringAsFixed(2);
+        break;
+      case 'ADA':
+        adaController.text = _calculo(ada, 1, real).toStringAsFixed(2);
+        break;
+      case 'UNI':
+        uniController.text = _calculo(uni, 1, real).toStringAsFixed(2);
+        break;
+      case 'USDC':
+        usdcController.text = _calculo(usdc, 1, real).toStringAsFixed(2);
+        break;
     }
   }
 
@@ -166,27 +182,78 @@ class _HomePageState extends State<HomePage> {
       _clearAll();
       return;
     }
+    double coin = double.parse(text);
+    realController.text = _calculo(1, btc, coin).toStringAsFixed(2);
+  }
+
+  void _ltcChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double coin = double.parse(text);
+    realController.text = _calculo(1, ltc, coin).toStringAsFixed(2);
+  }
+
+  void _adaChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double coin = double.parse(text);
+    realController.text = _calculo(1, ada, coin).toStringAsFixed(2);
+  }
+
+  void _uniChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double coin = double.parse(text);
+    realController.text = _calculo(1, uni, coin).toStringAsFixed(2);
+  }
+
+  void _usdcChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double coin = double.parse(text);
+    realController.text = _calculo(1, usdc, coin).toStringAsFixed(2);
   }
 
   void carregaCryptos(data) {
-    btc = data![0];
-    ltc = data![1];
-    ada = data![2];
-    uni = data![3];
-    usdc = data![4];
+    btc = double.parse(data![0].ticker.buy);
+    ltc = double.parse(data![1].ticker.buy);
+    ada = double.parse(data![2].ticker.buy);
+    uni = double.parse(data![3].ticker.buy);
+    usdc = double.parse(data![4].ticker.buy);
 
-    btcController.text = btc!.ticker.buy;
-    ltcController.text = ltc!.ticker.buy;
-    adaController.text = ada!.ticker.buy;
-    uniController.text = uni!.ticker.buy;
-    usdcController.text = usdc!.ticker.buy;
-    realController.text = 1.toString();
+    switch (_dropdownController) {
+      case 'BTC':
+        realController.text = btc.toStringAsFixed(2);
+        break;
+      case 'LTC':
+        realController.text = ltc.toStringAsFixed(2);
+        break;
+      case 'ADA':
+        realController.text = ada.toStringAsFixed(2);
+        break;
+      case 'UNI':
+        realController.text = uni.toStringAsFixed(2);
+        break;
+      case 'USDC':
+        realController.text = usdc.toStringAsFixed(2);
+        break;
+    }
   }
 }
 
-Widget buildTextField(TextEditingController c, String prefix, Function f) {
+Widget buildTextField(
+    TextEditingController c, String prefix, Function(String) f) {
   return TextField(
     controller: c,
+    onChanged: f,
     decoration: InputDecoration(
       prefixText: prefix,
       border: const OutlineInputBorder(),
---
+++ lib/widget/loading.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  _loading() {
+    return Column(
+      children: const <Widget>[
+        CircularProgressIndicator(
+          color: Colors.amber,
+        ),
+        Divider(
+          height: 20,
+          color: Colors.white,
+        ),
+        Text(
+          "Carregando Dados...",
+          style: TextStyle(color: Colors.black, fontSize: 25.0),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [_loading()],
+    );
+  }
+}
